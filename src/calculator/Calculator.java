876b52f257f90e745e2ca089d53ed814a8cd1a8f
Jurijs <jurijsmaskaluns@gmail.com>
2018-02-15 16:33:11 +0200

    calculation compact

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -16,86 +16,72 @@ public class Calculator {
         double calc;
         String op1 = expression[1];
         double result;
+        double c;
+        String op2;
         int i;
         i = expression.length;
-        System.out.println("kol-vo simvolov v stroke " + i);
         if (i == 3) {
-            switch (op1) {
-                case "+":
-                    result = a + b;
-                    break;
-                case "-":
-                    result = a - b;
-                    break;
+            op2 = "+";
+            c = 0;
+        } else {
+            c = Double.parseDouble(expression[4]);
+            op2 = expression[3];
+        }
+        if ((op1.equals("+") || op1.equals("-")) && (op2.equals("*") || op2.equals("/"))) {
+            System.out.println("op1: +-; op2: */");
+            switch (op2) {
                 case "*":
-                    result = a * b;
+                    calc = b * c;
                     break;
                 case "/":
-                    result = a / b;
+                    calc = b / c;
                     break;
                 default:
-                    return "error 1";
+                    return "error 2";
             }
-
+            switch (op1) {
+                case "+":
+                    result = a + calc;
+                    break;
+                case "-":
+                    result = a - calc;
+                    break;
+                default:
+                    return "error 3";
+            }
         } else {
-            double c = Double.parseDouble(expression[4]);
-            String op2 = expression[3];
-            if ((op1.equals("+") || op1.equals("-")) && (op2.equals("*") || op2.equals("/"))) {
-                System.out.println("op1: +-; op2: */");
-                switch (op2) {
-                    case "*":
-                        calc = b * c;
-                        break;
-                    case "/":
-                        calc = b / c;
-                        break;
-                    default:
-                        return "error 2";
-                }
-                switch (op1) {
-                    case "+":
-                        result = a + calc;
-                        break;
-                    case "-":
-                        result = a - calc;
-                        break;
-                    default:
-                        return "error 3";
-                }
-            } else {
-                System.out.println("op1: +-*/; op2: +-");
-                switch (op1) {
-                    case "+":
-                        calc = a + b;
-                        break;
-                    case "-":
-                        calc = a - b;
-                        break;
-                    case "*":
-                        calc = a * b;
-                        break;
-                    case "/":
-                        calc = a / b;
-                        break;
-                    default:
-                        return "error 4";
-                }
-                switch (op2) {
-                    case "+":
-                        result = calc + c;
-                        break;
-                    case "-":
-                        result = calc - c;
-                        break;
-                    case "*":
-                        result = calc * c;
-                        break;
-                    case "/":
-                        result = calc / c;
-                        break;
-                    default:
-                        return "error 5";
-                }
+            System.out.println("op1: +-*/; op2: +-");
+            switch (op1) {
+                case "+":
+                    calc = a + b;
+                    break;
+                case "-":
+                    calc = a - b;
+                    break;
+                case "*":
+                    calc = a * b;
+                    break;
+                case "/":
+                    calc = a / b;
+                    break;
+                default:
+                    return "error 4";
+            }
+            switch (op2) {
+                case "+":
+                    result = calc + c;
+                    break;
+                case "-":
+                    result = calc - c;
+                    break;
+                case "*":
+                    result = calc * c;
+                    break;
+                case "/":
+                    result = calc / c;
+                    break;
+                default:
+                    return "error 5";
             }
         }
         return String.valueOf(result);
